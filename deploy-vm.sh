100aa0765b1f4865a9ebbc7d1d8b60b6e7091591
Tosin Akinosho <takinosh@redhat.com>
2023-04-18 14:53:11 -0400

    updating

--- deploy-vm.sh
+++ deploy-vm.sh
@@ -1,8 +1,14 @@
 #!/bin/bash 
 set -e 
-if [ -z "$VM_NAME" ];
-then 
-    echo "Please provide the name of the VM to deploy"
+if [ -z "$VM_NAME" ]; then
+    echo "Error: Please provide the name of the VM to deploy by setting the VM_NAME environment variable."
+    echo "Example: export VM_NAME=my-vm"
+    exit 1
+fi
+
+if [ -z "$ACTION" ]; then
+    echo "Error: Please provide an action to perform by setting the ACTION environment variable to create, delete, or deploy_app."
+    echo "Example: export ACTION=deploy_app"
     exit 1
 fi
 
@@ -19,6 +25,6 @@ elif [ $ACTION == "deploy_app" ];
 then 
   #sudo kcli scp /tmp/manifest_tower-dev_20230325T132029Z.zip device-edge-workshops:/tmp
   #./setup-demo-infra.sh
+  echo "deploy app"
 fi
-
 
--- freeipa-server-container/configure-kcli-profile.sh
+++ freeipa-server-container/configure-kcli-profile.sh
@@ -20,7 +20,9 @@ PASSWORD=$(yq eval '.admin_user_password' "${ANSIBLE_VAULT_FILE}")
 VM_NAME=freeipa-server-container-$(echo $RANDOM | md5sum | head -c 5; echo;)
 IMAGE_NAME=Fedora-Cloud-Base-37-1.7.x86_64.qcow2
 DNS_FORWARDER=$(yq eval '.dns_forwarder' "${ANSIBLE_ALL_VARIABLES}")
+DOMAIN=$(yq eval '.domain' "${ANSIBLE_ALL_VARIABLES}")
 DISK_SIZE=50
+
 sudo rm -rf kcli-profiles.yml
 if [ -f ~/.kcli/profiles.yml ]; then
   sudo cp  ~/.kcli/profiles.yml kcli-profiles.yml
@@ -44,11 +46,14 @@ numcpus: 2
 memory: 4092
 net_name: ${NET_NAME} 
 reservedns: ${DNS_FORWARDER}
+domainname: ${DOMAIN}
 EOF
 
 sudo python3 profile_generator/profile_generator.py update_yaml freeipa-server-container freeipa-server-container/template.yaml  --vars-file /tmp/vm_vars.yaml
 cat  kcli-profiles.yml
 /usr/local/bin/ansiblesafe -f "${ANSIBLE_VAULT_FILE}" -o 1
+sudo cp kcli-profiles.yml ~/.kcli/profiles.yml
+sudo cp kcli-profiles.yml /root/.kcli/profiles.yml
 
 #echo "Creating VM ${VM_NAME}"
 #sudo kcli create vm -p freeipa-server-container ${VM_NAME} --wait
