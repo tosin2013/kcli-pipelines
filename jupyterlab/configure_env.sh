5187c01f937a7d81b2bdb6fe5befb848c6d2d9d4
Tosin Akinosho <takinosh@redhat.com>
2023-06-13 12:37:08 -0400

    updatig code

--- jupyterlab/configure_env.sh
+++ jupyterlab/configure_env.sh
@@ -6,7 +6,5 @@ sudo dnf install ncurses-devel curl -y
 curl 'https://vim-bootstrap.com/generate.vim' --data 'editor=vim&langs=javascript&langs=go&langs=html&langs=ruby&langs=python' > ~/.vimrc
 sudo python3 -m pip install --user --upgrade pip
 python3 -m pip install --user jupyterlab
-sudo firewall-cmd --add-port=8888/tcp --permanent
-sudo firewall-cmd --reload
 jupyter notebook --generate-config
 nohup jupyter-lab --no-browser --ip=0.0.0.0 --port=8888  | sudo tee /var/log/jupyter-lab.log #--NotebookApp.password=$DEFAULT_PASSWORD
