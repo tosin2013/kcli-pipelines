ddda17f0021d166cc6c31ab8e375a46a572838ce
Tosin Akinosho <takinosh@redhat.com>
2023-10-28 10:06:22 -0400

    adding deploy

---
+++ ceph-cluster/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash 
+
+if [ -f /opt/kcli-pipelines/helper_scripts/default.env ];
+then 
+  source /opt/kcli-pipelines/helper_scripts/default.env
+else
+  echo "default.env file does not exist"
+  exit 1
+fi
+
+if [ "$EUID" -ne 0 ]
+then 
+  export USE_SUDO="sudo"
+fi
+
+if [ ! -z "$CICD_PIPELINE" ]; then
+  export USE_SUDO="sudo"
+fi
+
+
+DOMAIN=$(yq eval '.domain' "${ANSIBLE_ALL_VARIABLES}")
+
+kcli create plan -f  ceph-cluster/ceph-cluster.yml
