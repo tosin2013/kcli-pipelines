03848c13f72344f18898d88b7db51fa40ec918cf
Tosin Akinosho <takinosh@redhat.com>
2023-11-02 11:27:16 -0400

    updating code

--- ceph-cluster/deploy.sh
+++ ceph-cluster/deploy.sh
@@ -20,4 +20,37 @@ fi
 
 DOMAIN=$(yq eval '.domain' "${ANSIBLE_ALL_VARIABLES}")
 
-kcli create plan -f  ceph-cluster/ceph-cluster.yml
+if [ ! -f /var/lib/libvirt/images/rhel9 ];
+then 
+  echo "RHEL 9 image does not exist"
+  echo "Run the following command to download the image"
+  echo "sudo kcli download image rhel9"
+  exit 1
+fi 
+
+if [  ! -f  /var/lib/libvirt/images/rhel-baseos-9.1-x86_64-kvm.qcow2 ];
+then 
+  sudo cp /var/lib/libvirt/images/rhel9   /var/lib/libvirt/images/rhel-baseos-9.1-x86_64-kvm.qcow2
+fi 
+
+cat ceph-cluster/ceph-cluster.yml
+kcli create plan -f  /opt/kcli-pipelines/ceph-cluster/ceph-cluster.yml
+
+# Loop over each VM in the ceph-cluster.yml file
+for VM_NAME in $(yq eval '. | keys | .[]' ceph-cluster.yml); do
+  # Get the IP address of the VM
+  IP_ADDRESS=$(sudo kcli info vm $VM_NAME $VM_NAME | grep ip: | awk '{print $2}' | head -1)
+  echo "VM $VM_NAME created with IP address $IP_ADDRESS"
+  
+  # Update the DNS using the add_ipa_entry.yaml playbook
+  $ANSIBLE_PLAYBOOK helper_scripts/add_ipa_entry.yaml \
+    --vault-password-file "$HOME"/.vault_password \
+    --extra-vars "@${ANSIBLE_VAULT_FILE}" \
+    --extra-vars "@${ANSIBLE_ALL_VARIABLES}" \
+    --extra-vars "key=${VM_NAME}" \
+    --extra-vars "freeipa_server_fqdn=idm.${DOMAIN_NAME}" \
+    --extra-vars "value=${IP_ADDRESS}" \
+    --extra-vars "freeipa_server_domain=${DOMAIN_NAME}" \    --extra-vars "action=present" -vvv
+
+done
+
