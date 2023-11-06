513264753cd1189c635b84462687af8927b167de
Tosin Akinosho <takinosh@redhat.com>
2023-11-06 08:46:44 -0500

    updating download

--- ceph-cluster/ceph-mon01.sh
+++ ceph-cluster/ceph-mon01.sh
@@ -16,7 +16,7 @@ dnf update -y
 subscription-manager repos --enable=rhceph-6-tools-for-rhel-9-x86_64-rpms
 dnf install ansible-core cephadm-ansible bind-utils -y
 
-curl https://raw.githubusercontent.com/tosin2013/kcli-pipelines/main/ceph-cluster/rhel9_ceph.sh --output /tmp/rhel8_ceph.sh
+curl https://raw.githubusercontent.com/tosin2013/kcli-pipelines/main/ceph-cluster/rhel9_ceph.sh --output /tmp/rhel9_ceph.sh
 
 chmod +x /tmp/rhel9_ceph.sh
 
