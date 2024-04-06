453cd7231d6b409cd9e71c9102d3e9b160f47585
Tosin Akinosho <takinosh@redhat.com>
2024-04-06 11:21:40 -0400

    updating harbor

--- harbor/configure-kcli-profile.sh
+++ harbor/configure-kcli-profile.sh
@@ -13,54 +13,35 @@ else
 fi
 
 cd $KCLI_SAMPLES_DIR
-
+IMAGE_NAME=ubuntu
 
 /usr/local/bin/ansiblesafe -f "${ANSIBLE_VAULT_FILE}" -o 2
 PASSWORD=$(yq eval '.admin_user_password' "${ANSIBLE_VAULT_FILE}")
-RHSM_ORG=$(yq eval '.rhsm_org' "${ANSIBLE_VAULT_FILE}")
-RHSM_ACTIVATION_KEY=$(yq eval '.rhsm_activationkey' "${ANSIBLE_VAULT_FILE}")
-PULL_SECRET=$(yq eval '.openshift_pull_secret' "${ANSIBLE_VAULT_FILE}")
-sudo echo ${PULL_SECRET} | sudo tee pull-secret.json  > /dev/null
-VM_NAME=harbor-$(echo $RANDOM | md5sum | head -c 5; echo;)
-IMAGE_NAME=rhel9
+VM_NAME=ubuntu-$(echo $RANDOM | md5sum | head -c 5; echo;)
 DNS_FORWARDER=$(yq eval '.dns_forwarder' "${ANSIBLE_ALL_VARIABLES}")
-DOMAIN=$(yq eval '.domain' "${ANSIBLE_ALL_VARIABLES}")
-DISK_SIZE=350
+DISK_SIZE=120
 KCLI_USER=$(yq eval '.admin_user' "${ANSIBLE_ALL_VARIABLES}")
-sudo rm -rf kcli-profiles.yml
+DOMAIN=$(yq eval '.domain' "${ANSIBLE_ALL_VARIABLES}")
 if [ -f /home/${KCLI_USER}/.kcli/profiles.yml ]; then
   sudo cp  /home/${KCLI_USER}/.kcli/profiles.yml kcli-profiles.yml
 else 
     sudo mkdir -p /home/${KCLI_USER}/.kcli
     sudo mkdir -p /root/.kcli
 fi
-if [ -d /home/${KCLI_USER}/.generated/vmfiles ]; then
-  echo "generated directory already exists"
-else
-  sudo mkdir -p  /home/${KCLI_USER}/.generated/vmfiles
-  sudo mkdir -p  /root/.generated/vmfiles
-fi
-
 
 cat >/tmp/vm_vars.yaml<<EOF
 image: ${IMAGE_NAME}
-user: cloud-user
 user_password: ${PASSWORD}
 disk_size: ${DISK_SIZE} 
 numcpus: 4
-memory: 8184
+memory: 8192
 net_name: ${NET_NAME} 
 reservedns: ${DNS_FORWARDER}
-domainname: ${DOMAIN}
-offline_token: ${OFFLINE_TOKEN}
-rhnorg: ${RHSM_ORG}
-rhnactivationkey: ${RHSM_ACTIVATION_KEY} 
+domain: ${DOMAIN}
 EOF
-sudo python3 profile_generator/profile_generator.py update-yaml harbor harbor/template.yaml  --vars-file /tmp/vm_vars.yaml
+
+sudo python3 profile_generator/profile_generator.py update-yaml ubuntu ubuntu/template.yaml  --vars-file /tmp/vm_vars.yaml
 #cat  kcli-profiles.yml
 /usr/local/bin/ansiblesafe -f "${ANSIBLE_VAULT_FILE}" -o 1
 sudo cp kcli-profiles.yml /home/${KCLI_USER}/.kcli/profiles.yml
 sudo cp kcli-profiles.yml /root/.kcli/profiles.yml
-sudo cp  pull-secret.json  /home/${KCLI_USER}/.generated/vmfiles
-sudo cp pull-secret.json /root/.generated/vmfiles
-sudo rm pull-secret.json
