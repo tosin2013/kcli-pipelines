29c4be28306a5036b3895106f6a2c02b74072f63
Tosin Akinosho <takinosh@redhat.com>
2024-04-06 21:53:52 -0400

    updating scripts

--- step-ca-server/configure-step-ca-local.sh
+++ step-ca-server/configure-step-ca-local.sh
@@ -11,13 +11,6 @@ fi
 DOMAIN=$1
 DNS_IP=$2
 
-cd /tmp/ && curl -OL https://raw.githubusercontent.com/tosin2013/openshift-4-deployment-notes/master/pre-steps/configure-openshift-packages.sh
-chmod +x /tmp/configure-openshift-packages.sh && /tmp/configure-openshift-packages.sh -i
-wget https://dl.smallstep.com/cli/docs-ca-install/latest/step-cli_amd64.rpm && sudo rpm -i step-cli_amd64.rpm
-wget https://dl.smallstep.com/certificates/docs-ca-install/latest/step-ca_amd64.rpm && sudo rpm -i step-ca_amd64.rpm
-ansible-galaxy collection install maxhoesel.smallstep>=0.25.2
-sudo hostnamectl set-hostname step-ca.${DOMAIN}
-
 if [ -f /tmp/initial_password ]; then
     mkdir -p /etc/step
     cp /tmp/initial_password /etc/step/initial_password
@@ -31,9 +24,22 @@ if [ ! -f /etc/step/initial_password ]; then
     echo "$password" > /etc/step/initial_password
 fi
 
+if ! command -v oc &> /dev/null; then
+    echo "oc command not found. Installing OpenShift CLI..."
+    # Add code here to install OpenShift CLI
+    cd /tmp/ && curl -OL https://raw.githubusercontent.com/tosin2013/openshift-4-deployment-notes/master/pre-steps/configure-openshift-packages.sh
+    chmod +x /tmp/configure-openshift-packages.sh && /tmp/configure-openshift-packages.sh -i
+fi
+
+wget https://dl.smallstep.com/cli/docs-ca-install/latest/step-cli_amd64.rpm && sudo rpm -i step-cli_amd64.rpm
+wget https://dl.smallstep.com/certificates/docs-ca-install/latest/step-ca_amd64.rpm && sudo rpm -i step-ca_amd64.rpm
+ansible-galaxy collection install maxhoesel.smallstep>=0.25.2
+sudo hostnamectl set-hostname step-ca.${DOMAIN}
+
+
 step ca init  --dns=step-ca.${DOMAIN} --address='[::]:443'  \
     --address=0.0.0.0:443  --name="Certificate authority for internal.${DOMAIN}" \
-    --deployment-type=standalone --provisioner="root@internal.${DOMAIN} " --password-file=/etc/step/initial_password
+    --deployment-type=standalone --provisioner="root@internal.${DOMAIN} " --password-file=/etc/step/initial_password || exit $?
 
 step ca provisioner add acme --type ACME
 
@@ -46,3 +52,9 @@ jq '.authority.provisioners[0].claims = {
 nohup step-ca $(step path)/config/ca.json --password-file=/etc/step/initial_password > step-ca.log 2>&1 &
 cat step-ca.log
 echo "step-ca $(step path)/config/ca.json"
+
+export interface_name="System eth0"
+sudo nmcli connection modify  "${interface_name}"  ipv4.dns $DNS_IP,1.1.1.1
+sudo nmcli connection down "${interface_name}" && sudo nmcli connection up "${interface_name}"
+# list the dns information using nmcli 
+sudo nmcli connection show "${interface_name}" | grep ipv4.dns
