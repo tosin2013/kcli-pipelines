9aa0e82b04d1db44365cd3abfb22c7d8e414fe11
Tosin Akinosho <takinosh@redhat.com>
2024-04-09 12:44:44 -0400

    updating scripts

--- mirror-registry/configure-quay.sh
+++ mirror-registry/configure-quay.sh
@@ -10,19 +10,20 @@ fi
 VERSION=${1}
 DOMAIN=${2}
 
-dnf update -y
-dnf install curl wget tar jq skopeo httpd-tools podman openssl nano nfs-utils bash-completion bind-utils ansible-core vim libvirt firewalld acl policycoreutils-python-utils -y
+sudo dnf update -y
+sudo dnf install curl wget tar jq skopeo httpd-tools podman openssl nano nfs-utils bash-completion bind-utils ansible-core vim libvirt firewalld acl policycoreutils-python-utils -y
 
 if ! grep -q "0" /proc/sys/net/ipv4/ip_unprivileged_port_start; then
-    echo 0 > "/proc/sys/net/ipv4/ip_unprivileged_port_start"
+    sudo tee "/proc/sys/net/ipv4/ip_unprivileged_port_start" <<< "0"
 fi
 
 if [  ! -f $HOME/mirror-registry-offline.tar.gz ];
 then
+    cd $HOME
     curl -OL "https://github.com/quay/mirror-registry/releases/download/${VERSION}/mirror-registry-offline.tar.gz"
     tar -zxvf mirror-registry-offline.tar.gz
 fi 
-mkdir -p /registry/
+sudo mkdir -p /registry/
 #systemctl start firewalld
 #systemctl enable firewalld
 #firewall-cmd --add-port=8443/tcp --permanent
@@ -37,6 +38,5 @@ fi
 
 if [ ! -s "/root/.generated/vmfiles/mirror-registry.${DOMAIN}.crt" ]; then
     echo "Installing mirror-registry without self-signed certificate"
-    ./mirror-registry install  --quayRoot /registry/ --quayHostname mirror-registry.${DOMAIN}
-  --certPath mirror-registry.${DOMAIN}.crt --sslKey /home/${USER}/mirror-registry.${DOMAIN}.key || tee /tmp/mirror-registry-offline.log
+    ./mirror-registry install  --quayRoot /registry/ --quayHostname mirror-registry.${DOMAIN} --certPath mirror-registry.${DOMAIN}.crt --sslKey /home/${USER}/mirror-registry.${DOMAIN}.key || tee /tmp/mirror-registry-offline.log
 fi
