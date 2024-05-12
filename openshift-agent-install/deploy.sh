7dd41f7b048a29376cf9d0c188fab47d1111325a
Tosin Akinosho <takinosh@redhat.com>
2024-05-11 20:09:13 -0400

    updating deploy.sh

--- openshift-agent-install/deploy.sh
+++ openshift-agent-install/deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash 
-export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
-set -xe
+
 if [ -f /opt/kcli-pipelines/helper_scripts/default.env ];
 then 
   source /opt/kcli-pipelines/helper_scripts/default.env
@@ -9,15 +8,15 @@ else
   exit 1
 fi
 
-if [ ! -d /opt/ocp4-disconnected-helper ];
+if [ ! -d /opt/kcli-openshift4-baremetal ];
 then 
     cd /opt/
-    git clone https://github.com/tosin2013/openshift-agent-install.git
-    cd openshift-agent-install
+    git clone https://github.com/karmab/kcli-openshift4-baremetal
+    cd kcli-openshift4-baremetal
 else
-    cd /opt/openshift-agent-install
+    cd /opt/kcli-openshift4-baremetal
     git config pull.rebase false
-    git config --global --add safe.directory /opt/openshift-agent-install
+    git config --global --add safe.directory /opt/kcli-openshift4-baremetal
     git pull
 fi 
 
@@ -30,64 +29,44 @@ if [ ! -z "$CICD_PIPELINE" ]; then
   export USE_SUDO="sudo"
 fi
 
-if [ ! -z "$OPENSHIFT_VERSION" ]; then 
-  export OPENSHIFT_VERSION="4.14.1"
-fi
-
-if command -v nmstate &>/dev/null; then
-  echo "nmstate is installed"
-else
-  echo "nmstate is not installed"
-  ${USE_SUDO} dnf install nmstate -y 
-fi
-
-
-${USE_SUDO} /usr/local/bin/ansible-galaxy install -r openshift-agent-install/collections/requirements.yml
-
-# Assign arguments to variables
-DOMAIN=$(yq eval '.domain' "${ANSIBLE_ALL_VARIABLES}")
-# 98c4fd93ab54e1555a911d33c5bb9c1283905cf215d72038eb856f6efda1c6f1
-GRAPH_DATA_IMAGE=$(yq eval '.graph_data_image' "${ANSIBLE_ALL_VARIABLES}")
-new_graphDataImage="harbor.${DOMAIN}/oc-mirror/openshift/graph-image@sha256:98c4fd93ab54e1555a911d33c5bb9c1283905cf215d72038eb856f6efda1c6f1"
-
-new_releases="harbor.${DOMAIN}/oc-mirror/openshift/release-images"
-
-# File to be updated
-updateservice_file="openshift-agent-install/templates/updateservice.yml.j2"
-file="example_vars/kcli-pipeline-vars.yaml"
-
-
-
-# Use yq to update the graphDataImage and releases fields
-yq e ".spec.graphDataImage = \"$new_graphDataImage\"" -i "$updateservice_file"
-yq e ".spec.releases = \"$new_releases\"" -i "$updateservice_file"
-
-# updating pipeline vars 
-yq e ".base_domain = \"$DOMAIN\"" -i "$file"
-yq e ".ssh_public_key_path = \"$new_value\"" -i "$file"
-yq e ".control_plane_replicas = \"$new_graphDataImage\"" -i "$file"
-yq e ".app_node_replicas = \"$new_releases\"" -i "$file"
-yq e ".openshift_version = \"$OPENSHIFT_VERSION\"" -i "$file"
-DNS_FORWARDER=$(sudo kcli info vm freeipa | grep ip: | awk '{print $2}' | head -1)
-sed -i "s|192.168.122.10|${DNS_FORWARDER}|g" "$file"
-sed -i "s|kemo.labs|${DOMAIN}|g" "$file"
-
-
-cd /opt/openshift-agent-install/openshift-agent-install
-rm -rf ./generated_manifests/lab
-cd /opt/openshift-agent-install/
-ansible-playbook openshift-agent-install/create-manifests.yml -e "@example_vars/kcli-pipeline-vars.yaml" -vv
-cat openshift-agent-install/generated_manifests/lab/agent-config.yaml
-openshift-install agent create image --dir   /opt/openshift-agent-install/openshift-agent-install/generated_manifests/lab
-
-if [ $KVM_DEPLOY == "TRUE" ];
+if [ ! -z ${ZONE_NAME} ];
 then 
-  sudo rm -rf /var/lib/libvirt/images/agent.x86_64.iso
-  cd /opt/openshift-agent-install/openshift-agent-install/generated_manifests/lab
-  sudo cp agent.x86_64.iso /var/lib/libvirt/images/
-  cd /opt/openshift-agent-install/
-fi 
+  DOMAIN=$(yq eval '.domain' "${ANSIBLE_ALL_VARIABLES}")
+else 
+  DOMAIN=${ZONE_NAME}
+fi
 
-bash -x baremetal-test-script.sh sno
-cd /opt/openshift-agent-install/openshift-agent-install/
-openshift-install agent wait-for bootstrap-complete --dir ./generated_manifests/lab --log-level debug
+function create(){
+    ${USE_SUDO} /usr/local/bin/ansiblesafe -f "${ANSIBLE_VAULT_FILE}" -o 2
+    #yq eval '.openshift_pull_secret' "${ANSIBLE_VAULT_FILE}" > openshift_pull.json
+    ${USE_SUDO} yq eval '.openshift_pull_secret' "${ANSIBLE_VAULT_FILE}" | sudo tee openshift_pull.json >/dev/null
+
+    #cat openshift_pull.json
+    ${USE_SUDO} ln -s /opt/qubinode_navigator/inventories/${TARGET_SERVER}/group_vars/control/${DEPLOYMENT_CONFIG}  lab.yml
+    ${USE_SUDO} yq eval ".domain = \"$DOMAIN\"" -i /opt/qubinode_navigator/inventories/${TARGET_SERVER}/group_vars/control/${DEPLOYMENT_CONFIG} || exit $?
+    ${USE_SUDO} /opt/kcli-pipelines/kcli-openshift4-baremetal/env-checks.sh  || exit $?
+    cat lab.yml
+    ${USE_SUDO} kcli create plan --paramfile  lab.yml lab
+}
+
+
+function destroy(){
+    ${USE_SUDO} kcli delete plan lab --y
+    ${USE_SUDO} rm -rf lab.yml
+    export VM_PROFILE=freeipa
+    export VM_NAME="freeipa"
+    export  ACTION="delete" # create, delete
+
+    /opt/kcli-pipelines/deploy-vm.sh
+}
+
+if [ $ACTION == "create" ];
+then 
+  create
+elif [ $ACTION == "delete" ]; 
+then 
+  destroy
+else 
+  echo "help"
+fi
+
