5adbdf9d43471acae3820e4a2178095243d68197
Tosin Akinosho <takinosh@redhat.com>
2024-08-07 18:11:25 -0400

    testing

--- setup_jekyll_docs.sh
+++ setup_jekyll_docs.sh
@@ -119,7 +119,7 @@ layout: default
 title: Home
 nav_order: 1
 description: "Welcome to the documentation for ${SITE_TITLE}."
-permalink: /${GITHUB_REPO}
+permalink: /
 ---
 
 # Welcome to ${SITE_TITLE} Documentation
