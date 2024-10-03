ab8dbe71abf967b2a545e14c91d90a3dac22d349
Tosin Akinosho <takinosh@redhat.com>
2024-10-03 09:28:56 -0400

    updating code

--- ansible-aap/configure-kcli-profile.sh
+++ ansible-aap/configure-kcli-profile.sh
@@ -34,18 +34,22 @@ KCLI_USER=$(yq eval '.admin_user' "${ANSIBLE_ALL_VARIABLES}")
 #  https://access.redhat.com/downloads/content/480/ver=2.4/rhel---9/2.4/x86_64/product-software
 PROVIDED_SHA_VALUE=7c4509b3436c7423a60a65815493b3d66162acd09dbca131a9b5edad9e319a40 #$(yq eval '.provided_sha_value' "${ANSIBLE_ALL_VARIABLES}")
 #sudo rm -rf kcli-profiles.yml
-if [ -f /home/${KCLI_USER}/.kcli/profiles.yml ]; then
-  sudo cp  /home/${KCLI_USER}/.kcli/profiles.yml kcli-profiles.yml
+if [ -f /home/${USER}/.kcli/profiles.yml ]; then
+  sudo cp  /home/${USER}/.kcli/profiles.yml kcli-profiles.yml
 else 
     sudo mkdir -p /home/${KCLI_USER}/.kcli
+    sudo mkdir -p /home/${USER}/.kcli
     sudo mkdir -p /root/.kcli
 fi
-if [ -d /home/${KCLI_USER}/.generated/vmfiles ]; then
+
+if [ -d /home/${USER}/.generated/vmfiles ]; then
   echo "generated directory already exists"
 else
   sudo mkdir -p  /home/${KCLI_USER}/.generated/vmfiles
+  sudo mkdir -p  /home/${USER}/.generated/vmfiles
   sudo mkdir -p  /root/.generated/vmfiles
 fi
+
 
 cat >/tmp/vm_vars.yaml<<EOF
 image: rhel9
@@ -67,6 +71,7 @@ EOF
 determine_command_yaml
 sudo python3 profile_generator/profile_generator.py $COMMAND ansible-aap ansible-aap/ansible-aap.yml  --vars-file /tmp/vm_vars.yaml
 ####cat  kcli-profiles.yml
+sudo cp kcli-profiles.yml /home/${USER}/.kcli/profiles.yml
 sudo cp kcli-profiles.yml /home/${KCLI_USER}/.kcli/profiles.yml
 sudo cp kcli-profiles.yml /root/.kcli/profiles.yml
 /usr/local/bin/ansiblesafe -f "${ANSIBLE_VAULT_FILE}" -o 1
