53b5a41ac29a1002b47ca5a40e736262d09e168f
Tosin Akinosho <takinosh@redhat.com>
2024-10-02 22:08:42 -0400

    testing

--- configure-kcli-profiles.sh
+++ configure-kcli-profiles.sh
@@ -1,6 +1,39 @@
 #!/bin/bash
 #set -x
 #export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+
+# Function to get OS and version
+get_os_version() {
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        OS=$ID
+        VERSION_ID=$VERSION_ID
+    else
+        echo "Cannot determine OS version. /etc/os-release not found."
+        exit 1
+    fi
+}
+
+
+# Function to determine the command based on the OS and version
+determine_command_yaml() {
+    get_os_version
+
+    if [[ "$OS" == "centos" || "$OS" == "rhel" || "$OS" == "rocky" ]]; then
+        if [[ "$VERSION_ID" == 8* ]]; then
+            COMMAND="update-yaml"
+        elif [[ "$VERSION_ID" == 9* ]]; then
+            COMMAND="update_yaml"
+        else
+            echo "Unsupported version: $VERSION_ID"
+            exit 1
+        fi
+    else
+        echo "Unsupported OS: $OS"
+        exit 1
+    fi
+}
+
 GIT_REPO=https://github.com/tosin2013/kcli-pipelines.git
 
 if [ -z $TARGET_SERVER ];
