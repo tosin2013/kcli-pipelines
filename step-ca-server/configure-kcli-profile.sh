a8c5860e50ce664e9414fa9e836bbc68e1e04017
Tosin Akinosho <takinosh@redhat.com>
2024-10-07 12:14:21 -0400

    Refactor configure-kcli-profile.sh to determine the command based on the OS and version

--- step-ca-server/configure-kcli-profile.sh
+++ step-ca-server/configure-kcli-profile.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
 #export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 set -x
+
+# Function to get OS and version
+get_os_version() {
+    if [ -f /etc/os-release ]; then
+        . /etc/os-release
+        OS=$ID
+        VERSION_ID=$VERSION_ID
+    else
+        log "Cannot determine OS version. /etc/os-release not found."
+        exit 1
+    fi
+}
+
+# Function to determine the command based on the OS and version
+determine_command_yaml() {
+    get_os_version
+
+    if [[ "$OS" == "centos" || "$OS" == "rhel" || "$OS" == "rocky" ]]; then
+        if [[ "$VERSION_ID" == 8* ]]; then
+            COMMAND="update-yaml"
+        elif [[ "$VERSION_ID" == 9* ]]; then
+            COMMAND="update_yaml"
+        else
+            log "Unsupported version: $VERSION_ID"
+            exit 1
+        fi
+    else
+        log "Unsupported OS: $OS"
+        exit 1
+    fi
+}
+
 if [ -f ../helper_scripts/default.env ];
 then 
   source ../helper_scripts/default.env
