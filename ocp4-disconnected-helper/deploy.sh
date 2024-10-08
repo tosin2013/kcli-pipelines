e4f650ec6ee57f45e26df5b4b765431d722173d1
Tosin Akinosho <takinosh@redhat.com>
2024-10-08 11:45:30 -0400

    updating code

--- ocp4-disconnected-helper/deploy.sh
+++ ocp4-disconnected-helper/deploy.sh
@@ -97,6 +97,7 @@ then
   ${USE_SUDO} yq eval '.harbor_hostname = "harbor.'${GUID}.${DOMAIN}'"' -i output.yaml || exit $?
   IP_ADDRESS=$(${USE_SUDO} /usr/bin/kcli info vm harbor | grep ip: | awk '{print $2}')
 
+  SSH_PASSWORD=$(yq eval '.admin_user_password' "${ANSIBLE_VAULT_FILE}")
   ${USE_SUDO} sshpass -p "$SSH_PASSWORD" ${USE_SUDO} ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no cloud-user@${IP_ADDRESS} || exit $?
   ${USE_SUDO}  tee /tmp/inventory <<EOF
 ## Ansible Inventory template file used by Terraform to create an ./inventory file populated with the nodes it created
