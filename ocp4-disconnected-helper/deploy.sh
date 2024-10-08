5b83605acbb6f904a121c1aa37dbc0a1b44f2d9e
Tosin Akinosho <takinosh@redhat.com>
2024-10-08 11:17:05 -0400

    Refactor runner labels in deploy.sh to fix GUID variable interpolation

--- ocp4-disconnected-helper/deploy.sh
+++ ocp4-disconnected-helper/deploy.sh
@@ -61,14 +61,14 @@ fi
 # if SETUP_HARBER_REGISTRY is set to true, then run the playbook
 if [ "${SETUP_HARBER_REGISTRY}" == "true" ];
 then 
-  DOMAIN=${GUID}.$(yq eval '.domain' "${ANSIBLE_ALL_VARIABLES}")
+  DOMAIN=$(yq eval '.domain' "${ANSIBLE_ALL_VARIABLES}")
 
   cd  /opt/ocp4-disconnected-helper
 
   if ${USE_SUDO}  kcli list vms | grep -oq 'harbor'; then
      echo "Found 'harbor' in the output"
-     ${USE_SUDO}  kcli ssh harbor sudo  cat  /etc/letsencrypt/live/harbor.${DOMAIN}/fullchain.pem > /tmp/harbor.${DOMAIN}.bundle.crt
-     ${USE_SUDO}  kcli ssh harbor sudo  cat  /etc/letsencrypt/live/harbor.${DOMAIN}/privkey.pem > /tmp/harbor.${DOMAIN}.key
+     ${USE_SUDO}  kcli ssh harbor sudo  cat  /etc/letsencrypt/live/harbor.${GUID}.${DOMAIN}/fullchain.pem > /tmp/harbor.${GUID}.${DOMAIN}.bundle.crt
+     ${USE_SUDO}  kcli ssh harbor sudo  cat  /etc/letsencrypt/live/harbor.${GUID}.${DOMAIN}/privkey.pem > /tmp/harbor.${GUID}.${DOMAIN}.key
   else
       echo "'harbor' not found in the output"
       exit 1
@@ -80,11 +80,11 @@ then
   ${USE_SUDO} chown runner:users /tmp/output.json || exit $?
 
   # Load the certificate contents into a shell variable
-  certificate=$(${USE_SUDO} cat /tmp/harbor.${DOMAIN}.bundle.crt)
-  certificate_key=$(${USE_SUDO} cat harbor.${DOMAIN}.key)
+  certificate=$(${USE_SUDO} cat /tmp/harbor.${GUID}.${DOMAIN}.bundle.crt)
+  certificate_key=$(${USE_SUDO} cat harbor.${GUID}.${DOMAIN}.key)
 
-  ${USE_SUDO} cat /tmp/harbor.${DOMAIN}.bundle.crt > /dev/null 2>&1 || exit $?
-  ${USE_SUDO} cat harbor.${DOMAIN}.key > /dev/null 2>&1 || exit $?
+  ${USE_SUDO} cat /tmp/harbor.${GUID}.${DOMAIN}.bundle.crt > /dev/null 2>&1 || exit $?
+  ${USE_SUDO} cat harbor.${GUID}.${DOMAIN}.key > /dev/null 2>&1 || exit $?
 
   # Use jq to update the ssl_certificate field with the certificate
   ${USE_SUDO} jq --arg cert "$certificate" '.ssl_certificate = $cert' /tmp/output.json > /tmp/1.json || exit $?
@@ -94,7 +94,7 @@ then
 
   # Convert JSON back to YAML
   ${USE_SUDO} yq eval --output-format=yaml '.' /tmp/test_new.json > output.yaml || exit $?
-  ${USE_SUDO} yq eval '.harbor_hostname = "harbor.'${DOMAIN}'"' -i output.yaml || exit $?
+  ${USE_SUDO} yq eval '.harbor_hostname = "harbor.'${GUID}.${DOMAIN}'"' -i output.yaml || exit $?
   IP_ADDRESS=$(${USE_SUDO} /usr/bin/kcli info vm harbor | grep ip: | awk '{print $2}')
 
   ${USE_SUDO} sshpass -p "$SSH_PASSWORD" ${USE_SUDO} ssh-copy-id -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no cloud-user@${IP_ADDRESS} || exit $?
@@ -102,7 +102,7 @@ then
 ## Ansible Inventory template file used by Terraform to create an ./inventory file populated with the nodes it created
 
 [harbor]
-${VM_NAME}.${DOMAIN}
+${VM_NAME}.${GUID}.${DOMAIN}
 
 [all:vars]
 ansible_ssh_private_key_file=/root/.ssh/id_rsa
@@ -125,8 +125,8 @@ then
     then 
         ${USE_SUDO} rm -rf /opt/images/
     fi
-    DOMAIN=$(yq eval '.domain' "${GUID}.${ANSIBLE_ALL_VARIABLES}")
-    curl --fail https://harbor.${DOMAIN}/ || exit $?
+    DOMAIN=$(yq eval '.domain' "${ANSIBLE_ALL_VARIABLES}")
+    curl --fail https://harbor.${GUID}.${DOMAIN}/ || exit $?
     echo "Downloading images to /opt/images"
     cd  /opt/ocp4-disconnected-helper
     echo   ${USE_SUDO} /usr/local/bin/ansible-playbook -i /tmp/inventory /opt/ocp4-disconnected-helper/playbooks/download-to-tar.yml  -e "@extra_vars/download-to-tar-vars.yml" -vv
@@ -138,10 +138,10 @@ fi
 if [ "${PUSH_TAR_TO_REGISTRY}" == "true" ];
 then 
     DOMAIN=$(yq eval '.domain' "${ANSIBLE_ALL_VARIABLES}")
-    curl --fail https://harbor.${DOMAIN}/ || exit $?
+    curl --fail https://harbor.${GUID}.${DOMAIN}/ || exit $?
     echo "Pushing images to registry"
     cd  /opt/ocp4-disconnected-helper
-    ${USE_SUDO} yq eval '.registries[0].server = "harbor.'${DOMAIN}'"' -i extra_vars/push-tar-to-registry-vars.yml || exit $?
+    ${USE_SUDO} yq eval '.registries[0].server = "harbor.'${GUID}.${DOMAIN}'"' -i extra_vars/push-tar-to-registry-vars.yml || exit $?
     echo ${USE_SUDO} /usr/local/bin/ansible-playbook -i /tmp/inventory /opt/ocp4-disconnected-helper/playbooks/push-tar-to-registry.yml  -e "@extra_vars/push-tar-to-registry-vars.yml" -vv 
     ${USE_SUDO} /usr/local/bin/ansible-playbook -i /tmp/inventory /opt/ocp4-disconnected-helper/playbooks/push-tar-to-registry.yml  -e "@extra_vars/push-tar-to-registry-vars.yml" -vv || exit $?
     exit 0
