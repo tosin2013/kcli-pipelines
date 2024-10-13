733b4360126bca4bd1209f92604dc983eae5a0a5
Tosin Akinosho <takinosh@redhat.com>
2024-10-13 15:59:16 -0400

    Refactor harbor.sh to add support for CentOS, RHEL, and Rocky Linux

--- harbor/harbor.sh
+++ harbor/harbor.sh
@@ -4,6 +4,26 @@
 # https://github.com/goharbor/harbor/releases # v2.10.1
 set -x 
 
+if [[ "$OS" == "centos" || "$OS" == "rhel" || "$OS" == "rocky" ]]; then
+    if [[ "$VERSION_ID" == 8* ]]; then
+        ANSIBLE_PLAYBOOK="sudo -E /usr/local/bin/ansible-playbook"
+    elif [[ "$VERSION_ID" == 9* ]]; then
+        ANSIBLE_PLAYBOOK="sudo -E /usr/bin/ansible-playbook"
+    else
+        echo "Unsupported version: $VERSION_ID"
+        exit 1
+    fi
+fi
+
+if [ -f /opt/kcli-pipelines/helper_scripts/default.env ];
+then 
+  source /opt/kcli-pipelines/helper_scripts/default.env
+  source helper_scripts/helper_functions.sh
+else
+  echo "default.env file does not exist"
+  exit 1
+fi
+
 check_and_start_docker() {
     if ! command -v docker &> /dev/null; then
         # Docker is not installed, install it
@@ -115,3 +135,13 @@ sed -i "s|private_key: /your/private/key/path|private_key: /etc/letsencrypt/live
 cat harbor.yml
 ./install.sh
 echo -e "Harbor Installation Complete \n\nPlease log out and log in or run the command 'newgrp docker' to use Docker without sudo\n\nLogin to your harbor instance:\n docker login -u admin -p Harbor12345 $IPorFQDN"
+
+
+$ANSIBLE_PLAYBOOK /opt/kcli-pipelines/helper_scripts/add_ipa_entry.yaml \
+    --vault-password-file "$HOME"/.vault_password \
+    --extra-vars "@${ANSIBLE_VAULT_FILE}" \
+    --extra-vars "@${ANSIBLE_ALL_VARIABLES}" \
+    --extra-vars "key=harbor.${DOMAIN}" \
+    --extra-vars "freeipa_server_fqdn=idm.${DOMAIN}" \
+    --extra-vars "value=${IPorFQDN}" \
+    --extra-vars "freeipa_server_domain=${DOMAIN}" --extra-vars "action=present" -vvv || exit $?
