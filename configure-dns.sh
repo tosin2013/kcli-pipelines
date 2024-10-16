0733be3fe07e57b261d7ceef9bc3c9c0983430e2
Tosin Akinosho <takinosh@redhat.com>
2024-10-16 09:02:37 -0400

    Refactor configure-dns.sh to update DNS entries
    - Remove old DNS entries before adding new ones
    - Update the DNS entries with the IP address of the VM and additional DNS server
    
    Closes #123

--- configure-dns.sh
+++ configure-dns.sh
@@ -4,8 +4,15 @@ export vm_name="freeipa"
 export ip_address=$(sudo kcli info vm "$vm_name" "$vm_name" | grep ip: | awk '{print $2}' | head -1)
 export interface_name="bond0" # "System eth0"
 echo "VM $vm_name created with IP address $ip_address"
-sudo nmcli connection modify  "${interface_name}"  +ipv4.dns $ip_address,147.75.207.207
+
+# Remove old DNS entries
+sudo nmcli connection modify "${interface_name}" ipv4.dns ""
+
+# Add new DNS entries
+sudo nmcli connection modify "${interface_name}" ipv4.dns $ip_address,147.75.207.207
 sudo nmcli connection reload
-# list the dns information using nmcli 
+
+# List the DNS information using nmcli
 sudo nmcli connection show "${interface_name}" | grep ipv4.dns
 sudo systemctl restart NetworkManager
+
